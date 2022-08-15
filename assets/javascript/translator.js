f3393ce56ad4246e63405a62f837e600b376a827
Nisrine Maruan <85900780+nisrinemr1@users.noreply.github.com>
2022-08-15 21:10:44 +0200

    UX - setting languages tags to navbar

--- assets/javascript/translator.js
+++ assets/javascript/translator.js
@@ -1,4 +1,6 @@
-
+// ///////*******************************************///////
+// ///////************ CHANGE LANGUAGES *************///////
+// ///////*******************************************///////
 // INITIALIZATION OF THE LANGUAGE SWITCHER BY FINDING THE RIGHT JSON OF THE LANG
 function Translate() {
     //initialization
@@ -32,9 +34,35 @@ function Translate() {
     }
 }
 
+ //This function will be called when user click changing language ( hmtl => body onload="translate('en', 'lng-tag')" )
+ function translate(lng, tagAttr) {
+    var translate = new Translate();
+    translate.init(tagAttr, lng);
+    translate.process();
+}
 
 
+// GET THE ID OF THE LANGUAGES BUTTONS
+const enTranslator = document.getElementById('enTranslator');
+const frTranslator = document.getElementById('frTranslator');
 
+//WHEN THE DOM IS LOADED AND THE USER CLICK ON THE LANGUAGE, IT WILL CHANGE
+document.addEventListener("DOMContentLoaded", () => {
+
+    //This is id of HTML element (English) with attribute lng-tag
+    enTranslator.addEventListener('click', ()=>{
+
+        translate('en', 'lng-tag');
+
+    });
+
+    //This is id of HTML element (Khmer) with attribute lng-tag
+    frTranslator.addEventListener('click', ()=>{
+
+        translate('fr', 'lng-tag');
+
+    });
+});
 
 
 // ///////*******************************************///////
@@ -92,36 +120,5 @@ dropdowns.forEach(dropdown => {
             // ADD THE ACTIVE LANGUAGE TO SHOW UP THE LANGUAGE
         
         });
-    });
-});
-
-
-// TO CHANGE THE LANGUAGE TO ALL THE DOM 
-
- //This function will be called when user click changing language
- function translate(lng, tagAttr) {
-    var translate = new Translate();
-    translate.init(tagAttr, lng);
-    translate.process();
-}
-
-
-// GET THE ID OF THE LANGUAGES BUTTONS
-const enTranslator = document.getElementById('enTranslator');
-const frTranslator = document.getElementById('frTranslator');
-
-//WHEN THE DOM IS LOADED AND THE USER CLICK ON THE LANGUAGE, IT WILL CHANGE
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("DOMJ'SAIS PAS QUOI")
-    //This is id of HTML element (English) with attribute lng-tag
-    enTranslator.addEventListener('click', ()=>{
-        console.log("english here")
-        translate('en', 'lng-tag');
-    });
-    //This is id of HTML element (Khmer) with attribute lng-tag
-    frTranslator.addEventListener('click', ()=>{
-        console.log("french here")
-        translate('fr', 'lng-tag');
     });
 });
