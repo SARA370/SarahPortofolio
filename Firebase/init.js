713d65fa74511df51b77f304e9aa57450bce41fa
Sarah <sarahalrashid370@gmail.com>
2022-08-18 20:35:51 +0200

    services section added

---
+++ Firebase/init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCU4LSUBymvnBmQAYw1USVgpdR1gBS1zws",
+    authDomain: "website-portfolio-4e5c3.firebaseapp.com",
+    projectId: "website-portfolio-4e5c3",
+    storageBucket: "website-portfolio-4e5c3.appspot.com",
+    messagingSenderId: "118891533287",
+    appId: "1:118891533287:web:fe47d58a6bb906780d6342"
+  };
