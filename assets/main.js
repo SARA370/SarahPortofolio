0f455704940c0357253fade0d5e336adc1a5f2f2
Sarah <sarahalrashid370@gmail.com>
2022-08-21 05:04:51 +0200

    contact form

--- assets/main.js
+++ assets/main.js
@@ -243,48 +243,70 @@ inputs.forEach((input) => {
 });
 
 
-/////////***************** SUBMIT BUTTON ****************///////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyB6Izsku_qLcnr9lc0Ivj5eejB7-5FCaRE",
-    authDomain: "test-form-d3492.firebaseapp.com",
-    databaseURL: "https://test-form-d3492.firebaseio.com",
-    projectId: "test-form-d3492",
-    storageBucket: "test-form-d3492.appspot.com",
-    messagingSenderId: "222398070278",
-    appId: "1:222398070278:web:bf51f5c8a26dcfff9ecd87",
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB6Izsku_qLcnr9lc0Ivj5eejB7-5FCaRE",
+//     authDomain: "test-form-d3492.firebaseapp.com",
+//     databaseURL: "https://test-form-d3492.firebaseio.com",
+//     projectId: "test-form-d3492",
+//     storageBucket: "test-form-d3492.appspot.com",
+//     messagingSenderId: "222398070278",
+//     appId: "1:222398070278:web:bf51f5c8a26dcfff9ecd87",
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
   
-  // Refernece contactInfo collections
-  let contactInfo = firebase.database().ref("infos");
+//   // Refernece contactInfo collections
+//   let contactInfo = firebase.database().ref("infos");
   
-// listen for a submit
-document.querySelector(".form-container").addEventListener("submit", submitForm);
+// // listen for a submit
+// document.querySelector(".form-container").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-    e.preventDefault();
+// function submitForm(e) {
+//     e.preventDefault();
 
-    //   Get input Values
-    let name = document.querySelector(".name").value;
-    let email = document.querySelector(".email").value;
-    let number = document.querySelector(".number").value;
-    let message = document.querySelector(".message").value;
-    console.log(name, email, number, message);
+//     //   Get input Values
+//     let name = document.querySelector(".name").value;
+//     let email = document.querySelector(".email").value;
+//     let number = document.querySelector(".number").value;
+//     let message = document.querySelector(".message").value;
+//     console.log(name, email, number, message);
 
-    saveContactInfo(name, email, number, message);
-    document.querySelector(".contact-form").reset();
-}
+//     saveContactInfo(name, email, number, message);
+//     document.querySelector(".contact-form").reset();
+// }
 
-// Save infos to Firebase
-function saveContactInfo(name, email, number, message) {
-    let newContactInfo = contactInfo.push();
+// // Save infos to Firebase
+// function saveContactInfo(name, email, number, message) {
+//     let newContactInfo = contactInfo.push();
 
-    newContactInfo.set({
-        name: name,
-        email: email,
-        number: number,
-        message: message,
-    });
-}
+//     newContactInfo.set({
+//         name: name,
+//         email: email,
+//         number: number,
+//         message: message,
+//     });
+// }
---
+++ assets/smtp.js
@@ -0,0 +1,26 @@
+﻿/* SmtpJS.com - v3.0.0 */
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+
+/////////***************** SUBMIT BUTTON ****************///////
+
+
+let name = document.getElementById("Name").value;
+let email = document.getElementById("Email").value;
+let phone = document.getElementById("Phone").value;
+let message = document.getElementById("Message").value;
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "selenadam07@gmail.com",
+        Password : "BE11F2CEC841C2CD9BE1327B9FFC5F238807",
+        To : 'sarahalrashid370@gmail.com',
+        From : "selenadam07@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
